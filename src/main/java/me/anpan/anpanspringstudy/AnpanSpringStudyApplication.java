15208a392e1aee1d1297e80c5be53ec1b6d85f91
DESKTOP-49C2M5J\LEE <leechang0423@naver.com>
2019-02-25 21:00:52 +0900

    데이터 바인더 추상화 : propertyEditor

---
+++ src/main/java/me/anpan/anpanspringstudy/AnpanSpringStudyApplication.java
@@ -0,0 +1,13 @@
+package me.anpan.anpanspringstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnpanSpringStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnpanSpringStudyApplication.class, args);
+    }
+
+}
---
+++ src/main/java/me/anpan/anpanspringstudy/propertyeditor/EventEditor.java
@@ -0,0 +1,17 @@
+package me.anpan.anpanspringstudy.propertyeditor;
+
+import java.beans.PropertyEditorSupport;
+
+public class EventEditor extends PropertyEditorSupport {
+
+    @Override
+    public void setAsText(String text) throws IllegalArgumentException {
+        setValue(new Event(Integer.parseInt(text)));
+    }
+
+    @Override
+    public String getAsText() {
+        Event event = (Event) getValue();
+        return event.getId().toString();
+    }
+}
