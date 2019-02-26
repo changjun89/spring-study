bf4dc3246186e9a00b2a8bf23159a5f9dab54a59
DESKTOP-49C2M5J\LEE <leechang0423@naver.com>
2019-02-26 21:20:41 +0900

    Resource 추상

--- src/main/java/me/anpan/anpanspringstudy/converter_formatter/MemberController.java
+++ src/main/java/me/anpan/anpanspringstudy/converter_formatter/MemberController.java
@@ -1,6 +1,7 @@
 package me.anpan.anpanspringstudy.converter_formatter;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     @GetMapping("/member/{member}")
-    public String getEvent(@PathVariable Member member) {
+    public String getEvent(@ModelAttribute Member member) {
         return member.getId().toString();
     }
 }
---
+++ src/main/java/me/anpan/anpanspringstudy/resource/ResourceAppRunner.java
@@ -0,0 +1,27 @@
+package me.anpan.anpanspringstudy.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ResourceAppRunner implements ApplicationRunner {
+
+    @Autowired
+    ApplicationContext resourceLoader;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println(resourceLoader.getClass());
+        Resource resource = resourceLoader.getResource("classpath:file/test.txt");
+        System.out.println(resource.getClass());
+
+        System.out.println(resource.exists());
+        System.out.println(resource.getDescription());
+
+
+    }
+}
